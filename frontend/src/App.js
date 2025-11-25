d2d40799a837d7b9499a24bf638083a32a307182
ayeshashaaik <shaaikayesha519@gmail.com>
2025-11-25 11:31:42 +0530

    tuesday changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,14 @@
 import React from "react";
-import Navbar from "./Components/Home/Navbar";
-import Hero from "./Components/Home/Hero";
-import Footer from "./Components/Home/Footer";
-import StepsSection from "./Components/Home/StepsSection";
-import Testimonials from "./Components/Home/Testimonials";
-import GrownForFlavour from "./Components/Home/GrownForFlavour";
-import SliderWithHero from "./Components/Home/SliderWithHero";
-import FaqSection from "./Components/Home/FaqSection";
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";  // Import BrowserRouter
+import AllRoutes from "./Routes/AllRoutes";        // Import Routes
 
 export default function App() {
   return (
-    <div className="app-root">
-      <Navbar />
-      <Hero />
-      <StepsSection />
-      <Testimonials />
-      <GrownForFlavour />
-      <SliderWithHero/>
-      <FaqSection/>
-
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="app-root">
+        <AllRoutes />
+      </div>
+    </BrowserRouter>
   );
 }
--- frontend/src/Components/Home/AccountLinks.jsx
+++ frontend/src/Components/Home/AccountLinks.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import "./AccountLinks.css";
+import { Link } from "react-router-dom";
 
 export default function AccountLinks() {
   return (
     <div className="acct">
       <div className="delivery">📅 Your delivery schedule</div>
-      <a className="signin" href="#">
+      {/* <a className="signin" href="#">
         Sign in or create account
-      </a>
+      </a> */}
+      <Link className="signin" to="/login">
+  Sign in or create account
+</Link>
+
       <div className="avatar">👤</div>
     </div>
   );
