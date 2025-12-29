69900c7dfaac1dbdabaf344b709c65ef5b4d9c0c
unknown <maheshpalakunda@gmail.com>
2025-12-29 11:13:33 +0530

    data fetch

--- frontend/src/Components/Home/Navbar.jsx
+++ frontend/src/Components/Home/Navbar.jsx
@@ -43,7 +43,7 @@ export default function Navbar() {
     const fetchProducts = async () => {
       try {
         // const baseUrl = window.ENV?.BACKEND_API || 'http://localhost:8000';
-	const baseUrl = window.ENV?.BACKEND_API || 'htpp://72.60.219.208:8000';
+	const baseUrl = window.ENV?.BACKEND_API || 'http://72.60.219.208:8000';
         const res = await fetch(`${baseUrl}/api/public/products`);
         if (!res.ok) throw new Error("Failed to fetch products");
         const data = await res.json();
